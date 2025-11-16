56ac07e8d7f9cf51fda1297cc36efcdc149df556
collin-s46 <collin_steele@icloud.com>
2025-11-15 22:36:54 -0500

    first analysis -- travel time vs day [bar]

---
+++ data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Read CSV file
+df = pd.read_csv('traffic_data280.csv', index_col='Date')
+
+plt.figure(figsize=(10, 6))
+
+sns.barplot(x=df['Day'], y=df['Travel Time'])
+
+plt.title("Travel Time vs. Day of Week")
+plt.show()
+# Print the DataFrame
+#print(df.tail())
