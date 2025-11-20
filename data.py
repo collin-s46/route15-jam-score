07964585661d61792231e8f94834799627ec3256
collin-s46 <collin_steele@icloud.com>
2025-11-19 22:29:48 -0500

    add lineplot script

--- data.py
+++ data.py
@@ -8,9 +8,12 @@ df = pd.read_csv('traffic_data.csv', index_col='Date')
 
 plt.figure(figsize=(10, 6))
 
-sns.barplot(x=df['Day'], y=df['Travel Time'])
+#sns.barplot(x=df['Day'], y=df['Travel Time'])
 
-plt.title("Travel Time vs. Day of Week")
+#plt.title("Travel Time vs. Day of Week")
+
+sns.barplot(x=df['Departure'], y=df['Travel Time'], hue=df['Day'], palette='muted')
+sns.lineplot(x=df['Departure'], y=df['Travel Time'], legend=False)
 plt.show()
 # Print the DataFrame
 #print(df.tail())
